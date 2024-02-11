40121e93a0ecb3b10f5d24f1fe67aa12516031bf
Apoorv Gadiya <apoorvgadiya@gmail.com>
2024-02-11 18:27:49 +0530

    Font Commit
    Finally the font is working , now its time to code functionality into the editor

--- src/main_Package/main_page.java
+++ src/main_Package/main_page.java
@@ -7,12 +7,14 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.GraphicsEnvironment;
 import java.awt.Toolkit;
+import java.io.File;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import java.awt.Point;
 
 public class main_page {
 
@@ -36,8 +38,23 @@ public class main_page {
     }
 
     private void initialize() {
-    	
-    	GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+   
+    	 GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+    	 try {
+             // Replace "path/to/Roboto-Regular.ttf" with the actual path to your Roboto font file
+             Font RobotoMono_Bold = Font.createFont(Font.TRUETYPE_FONT, new File("src/fonts/RobotoMono-Bold.ttf"));
+             Font RobotoMono_Bold_Italic  = Font.createFont(Font.TRUETYPE_FONT, new File("src/fonts/RobotoMono-BoldItalic.ttf"));
+             Font RobotoMono_Italic = Font.createFont(Font.TRUETYPE_FONT, new File("src/fonts/RobotoMono-Italic.ttf"));
+             Font RobotoMono_Regular = Font.createFont(Font.TRUETYPE_FONT, new File("src/fonts/RobotoMono-Regular.ttf"));
+             
+             // Register the font with the graphics environment
+             ge.registerFont(RobotoMono_Bold);
+             ge.registerFont(RobotoMono_Bold_Italic);
+             ge.registerFont(RobotoMono_Italic);
+             ge.registerFont(RobotoMono_Regular);
+         } catch (Exception e) {
+             e.printStackTrace();
+         }
 
         Dimension screenSize = screenDetails();
         frmTexteditor = new JFrame();
@@ -53,16 +70,33 @@ public class main_page {
         panel.setLayout(new BorderLayout());
 
         JTextArea textArea = new JTextArea();
-        Font font = new Font("Roboto Mono", Font.PLAIN, 21);
-        textArea.setFont(font);
+        textArea.setBackground(new Color(0, 0, 0));
+        textArea.setCaretColor(new Color(255, 255, 255));
+        textArea.setForeground(new Color(255, 255, 255));
+        textArea.setLocation(new Point(10, 10));
+        Font font1 = new Font("Roboto Mono", Font.TRUETYPE_FONT, 51);
+        textArea.setFont(font1);
         textArea.setLineWrap(true);
         textArea.setWrapStyleWord(true);
 
         JScrollPane scrollPane = new JScrollPane(textArea);
         scrollPane.getVerticalScrollBar().setUI(new CustomScrollBarUI());
 
-        frmTexteditor.getContentPane().add(scrollPane, BorderLayout.CENTER);
+        
+       
 
+        // Load the Roboto font file
+       
+
+        // Get the names of all available fonts
+        String[] fontNames = ge.getAvailableFontFamilyNames();
+
+        // Print the names of all available fonts
+        System.out.println("Available Fonts:");
+        for (String fontName : fontNames) {
+            System.out.println(fontName);
+        }
+        frmTexteditor.getContentPane().add(scrollPane, BorderLayout.CENTER);
         frmTexteditor.setBounds(0, 0, screenSize.width, screenSize.height);
         ImageIcon icon = new ImageIcon("src/icons/icon.png");
         frmTexteditor.setIconImage(icon.getImage());
